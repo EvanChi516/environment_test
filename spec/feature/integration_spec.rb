2673499b79faf95df4d0b178cf451c5f86e68358
root <root@localhost>
2021-10-01 02:22:54 +0000

    Book Collection

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,11 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe 'Creating a book', type: :feature do
+RSpec.describe 'Creating a book with title', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+	fill_in 'Author', with: 'J.K. Rowling'
+	fill_in 'Price', with: '20'
+	fill_in 'Publish date', with: '1997-06-26'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+  end
+end
+
+RSpec.describe 'Creating a book with title and author', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+	fill_in 'Author', with: 'J.K. Rowling'
+	fill_in 'Price', with: '20'
+	fill_in 'Publish date', with: '1997-06-26'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+	expect(page).to have_content('J.K. Rowling')
+  end
+end
+
+RSpec.describe 'Creating a book with title, author, and price', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+	fill_in 'Author', with: 'J.K. Rowling'
+	fill_in 'Price', with: '20'
+	fill_in 'Publish date', with: '1997-06-26'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+	expect(page).to have_content('J.K. Rowling')
+	expect(page).to have_content('20')
+  end
+end
+
+RSpec.describe 'Creating a book with title, author, price, and date', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+	fill_in 'Author', with: 'J.K. Rowling'
+	fill_in 'Price', with: '20'
+	fill_in 'Publish date', with: '1997-06-26'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+	expect(page).to have_content('J.K. Rowling')
+	expect(page).to have_content('20')
+	expect(page).to have_content('1997-06-26')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -2,15 +2,60 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+	described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: '20', publish_date: '1997-06-26')
   end
 
-  it 'is valid with valid attributes' do
+  it 'title is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
+  it 'title is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+	described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: '20', publish_date: '1997-06-26')
+  end
+
+  it 'author is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'author is not valid without a name' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+	described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: '20', publish_date: '1997-06-26')
+  end
+
+  it 'price is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'price is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+	described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: '20', publish_date: '1997-06-26')
+  end
+
+  it 'publish date is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'publish date is not valid without a date' do
+    subject.publish_date = nil
     expect(subject).not_to be_valid
   end
 end
